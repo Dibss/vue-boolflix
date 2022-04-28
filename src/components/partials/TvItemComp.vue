e1f8b3ad148f99cefd944e39b5c1dedb76b37be7
dibss <ludovicad01@gmail.com>
2022-04-28 18:05:21 +0200

    .

--- src/components/partials/TvItemComp.vue
+++ src/components/partials/TvItemComp.vue
@@ -1,26 +1,28 @@
 <template>
-  <li id="tv-item">
-    <div class="img"
+  <li class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front"
       :style="{backgroundImage : `url(https://www.themoviedb.org/t/p/w342/${tv.poster_path})`}"
-    ></div>
-    <div class="d-none details">
-      <div class="f-bold">Titolo:
-        <span>{{tv.title}}{{tv.name}}</span>
-      </div>
-      <div class="f-bold" v-show="tv.original_title && tv.original_name !== tv.title && tv.name">Titolo originale:
-        <span>{{tv.original_title}}{{tv.original_name}}</span>
-      </div>
-      <div class="f-bold">Lingua originale:
-        <span class="language" :class="(tv.original_language == 'en')? 'flag-en' : (tv.original_language == 'it')? 'flag-it': 'flag-unknown'"></span>
-      </div>
-      <div class="f-bold">Voto:
-        <span>{{tv.vote_average}}</span>
-      </div>
-      <div class="f-bold">Overview:
-        <span>{{tv.overview}}</span>
+      ></div>
+      <div class="flip-card-back">
+        <div class="f-bold">Titolo:
+          <span>{{tv.title}}{{tv.name}}</span>
+        </div>
+        <div class="f-bold" v-show="tv.original_title && tv.original_name !== tv.title && tv.name">Titolo originale:
+          <span>{{tv.original_title}}{{tv.original_name}}</span>
+        </div>
+        <div class="f-bold">Lingua originale:
+          <span class="language" :class="(tv.original_language == 'en')? 'flag-en' : (tv.original_language == 'it')? 'flag-it': 'flag-unknown'"></span>
+        </div>
+        <div class="f-bold">Voto:
+          <span>{{tv.vote_average}}</span>
+        </div>
+        <div class="f-bold">Overview:
+          <span>{{tv.overview}}</span>
+        </div>
       </div>
     </div>
-  </li>
+  </li> 
 </template>
 
 <script>
@@ -36,32 +38,47 @@ methods : {
 </script>
 
 <style scoped lang='scss'>
+.flip-card {
+  background-color: transparent;
+  width: 225px;
+  height: 320px;
+  perspective: 1000px;
+}
 
-#tv-item{
-  width: calc(100% / 6);
-  margin: 0.3em;
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+.flip-card-back {
   background-color: #000;
-  height: 30%;
-  overflow: hidden;
-  .img{
-    height: 30vh;
-    width: 250px;
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-  }
-  .details{
-    color: #fff;
-    margin: 1em 0.5em;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    width: 250px;
-    overflow-x: hidden;
-    padding-right: 1em;
-    span{
-      margin: 0.2em;
-    }
-  }
+  color: white;
+  transform: rotateY(180deg);
+  padding: 1em;
+  overflow-y: auto;
 }
 
 div.f-bold{margin-bottom: 0.5em;}
@@ -71,14 +88,4 @@ div > span{font-weight: normal;}
 .flag-en::before{content: "	\01F1FA \01F1F8";}
 .flag-it::before{content: '	\01F1EE \01F1F9';}
 .flag-unknown::before{content : "\01F3F4 \200D \2620 \FE0F"}
-
-#tv-item:hover .img{
-  display: none;
-}
-
-#tv-item:hover .details{
-  display: unset;
-  display: flex;
-  flex-direction: column;
-}
 </style>
